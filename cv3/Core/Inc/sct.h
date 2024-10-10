3315cebb45934a9cbb4a92c809bc6ee59fbdbaeb
slavajda02 <slavajda@gmail.com>
2024-10-10 10:51:21 +0200

    cv3 half done

---
+++ cv3/Core/Inc/sct.h
@@ -0,0 +1,19 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 10, 2024
+ *      Author: 240648
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+#include "main.h"
+
+void sct_init(void);
+
+void sct_led(uint32_t value);
+
+void sct_value(uint32_t value);
+
+#endif /* INC_SCT_H_ */
